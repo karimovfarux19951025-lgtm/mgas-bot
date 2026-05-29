96a1cbfda122b0af70d7f0a85353075da4671dfb
karimovfarux19951025-lgtm <karimovfarux19951025@gmail.com>
2026-05-29 11:18:41 +0500

    Add files via upload

--- bot.py
+++ bot.py
@@ -5,7 +5,8 @@ from aiogram.fsm.storage.memory import MemoryStorage
 
 from config import BOT_TOKEN
 from database import init_db
-from handlers import user, admin
+import user_handler
+import admin_handler
 
 logging.basicConfig(level=logging.INFO)
 
@@ -15,8 +16,8 @@ async def main():
     bot = Bot(token=BOT_TOKEN)
     dp = Dispatcher(storage=MemoryStorage())
     
-    dp.include_router(admin.router)
-    dp.include_router(user.router)
+    dp.include_router(admin_handler.router)
+    dp.include_router(user_handler.router)
     
     print("✅ MGAS Bot ishga tushdi!")
     await dp.start_polling(bot)
